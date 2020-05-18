14e042f0a8a6368c62de51344151ef4745a0fddf
9Jannat8 <60793592+9Jannat8@users.noreply.github.com>
2020-05-18 13:11:12 +0600

    Add files via upload

---
+++ basic-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char s1[5]={'A','m','a','r','\0'};
+    char s2[]={'n','a','m','\0'};
+    char s3[]="Jannat";
+    char s4[]="amr nam \
+    joya";// \ dile multiple line kaj korbe akta string a
+
+    printf("s1=%s\n", s1);
+    printf("s2=%s\n", s2);
+    printf("s3=%s\n", s3);
+    printf("s4=%s\n", s4);
+
+}
